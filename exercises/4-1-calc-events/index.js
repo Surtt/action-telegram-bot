7916e184a913a97c84e1b1cefbfa97b18d81f980
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-09-29 20:37:38 +0300

    complete events task

--- exercises/4-1-calc-events/index.js
+++ exercises/4-1-calc-events/index.js
@@ -2,9 +2,55 @@ const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
 
-const logDbConnection = () => {
-  console.log('DB connected');
-}
+let firstNum = process.argv[2];
+let secondNum = process.argv[3];
+const operation = process.argv[4];
 
-myEmitter.addListener('connected', logDbConnection);
-myEmitter.emit('connected');
+myEmitter.on('add', (a, b) => {
+  const firstNum = parseFloat(a);
+  const secondNum = parseFloat(b);
+  myEmitter.emit('result', console.log(firstNum + secondNum));
+});
+
+myEmitter.on('multiply', (a, b) => {
+  const firstNum = parseFloat(a);
+  const secondNum = parseFloat(b);
+  myEmitter.emit('result', console.log(firstNum * secondNum));
+});
+
+myEmitter.on('subtraction', (a, b) => {
+  const firstNum = parseFloat(a);
+  const secondNum = parseFloat(b);
+  myEmitter.emit('result', console.log(firstNum - secondNum));
+});
+
+myEmitter.on('division', (a, b) => {
+  const firstNum = parseFloat(a);
+  const secondNum = parseFloat(b);
+  myEmitter.emit('result', console.log(firstNum / secondNum));
+});
+
+switch (operation) {
+  case 'add': {
+    myEmitter.emit('add', firstNum, secondNum);
+    myEmitter.on('result', (result) => result);
+    break;
+  }
+  case 'multiply': {
+    myEmitter.emit('multiply', firstNum, secondNum);
+    myEmitter.on('result', (result) => result);
+    break;
+  }
+  case 'subtraction': {
+    myEmitter.emit('subtraction', firstNum, secondNum);
+    myEmitter.on('result', (result) => result);
+    break;
+  }
+  case 'division': {
+    myEmitter.emit('division', firstNum, secondNum);
+    myEmitter.on('result', (result) => result);
+    break;
+  }
+  default:
+    return operation;
+}
