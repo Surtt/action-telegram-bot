244cc4ce6b933f3f546a2d6c8cbbe3088410b6c0
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-10-15 21:22:57 +0300

    add typescript and db record

--- action-bot/prisma/seed.ts
+++ action-bot/prisma/seed.ts
@@ -2,17 +2,25 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const user = {
+interface IUser {
+  name: string;
+  city: string;
+}
+
+interface IAction {
+  name: string;
+  field: string;
+}
+
+const user: IUser = {
   name: 'Alex',
   city: 'Espoo',
 }
 
-const action = [
-  {
+const action: IAction = {
     name: 'React',
     field: 'Курсы',
-  }
-];
+};
 
 const main = async () => {
   await prisma.$connect();
@@ -21,4 +29,12 @@ const main = async () => {
   await prisma.$disconnect();
 }
 
-main();
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  });
