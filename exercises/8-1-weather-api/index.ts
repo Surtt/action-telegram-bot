b9c2e75a018443c1214353e08b901f89a476c8e7
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-11-05 22:41:22 +0200

    add weather api

--- exercises/8-1-weather-api/index.ts
+++ exercises/8-1-weather-api/index.ts
@@ -1,10 +1,30 @@
 import express, {Request, Response} from 'express';
+import 'dotenv/config';
+import axios from "axios";
 
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req: Request, res: Response) => {
-    res.send('Hello');
+app.get('/weather', async (req: Request, res: Response) => {
+    const token = process.env.TOKEN;
+    const city = req.query.city;
+    const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            q: city,
+            appid: token,
+            lang: 'ru',
+            units: 'metric',
+        }
+    });
+    const result = {
+        weather: data.weather[0].description,
+        temp: data.main.temp,
+        feels_like: data.main.feels_like,
+        pressure: data.main.pressure,
+        humidity: `${data.main.humidity}%`,
+        wind_speed: data.wind.speed,
+    }
+    res.json(result);
 })
 
 app.listen(port, () => {
